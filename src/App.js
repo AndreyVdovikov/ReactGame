20dae2657c7cfd7641847caec90524b6158d5a74
Andrey Vdovikov <52081051+AndreyVdovikov@users.noreply.github.com>
2020-06-22 12:11:57 +0300

    nc2

--- src/App.js
+++ src/App.js
@@ -5,36 +5,35 @@ class App extends React.Component {
   constructor (props) {
     super (props);
     this.state = {
-      squares:Array(15).fill(null),//ЗАПЯТАЯ БЛИН111
+      squares:Array(16).fill(null),//ЗАПЯТАЯ БЛИН111
 
 
     }
   }
-
   onClick = e => {
-    const index = +e.target.dataset.index;
-    this.setState(({ squares }) => ({
-      squares: squares.map((n, i) => i === index && !n
-        ? [ 'red', 'black' ][+(Math.random() < 0.9)]
-        : n
-      ),
-    }));
-  }
+     const index = +e.target.dataset.index;
+     this.setState(({ squares }) => ({
+       squares: squares.map((n, i) => i === index && !n
+         ? [ 'red', 'black' ][+(Math.random() < 0.7)]
+         : n
+       ),
+     }));
+   }
 
-  render() {
-    return (
-      <div className="ColorS">
-        {this.state.squares.map((n, i) => (
-          <div
-            className="ColorS-grid"
-            style={n ? { background: n } : null}
-            data-index={i}
-            onClick={this.onClick}
-          />
-        ))}
-      </div>
-    );
-  }
-}
+   render() {
+     return (
+       <div className="ColorS">
+         {this.state.squares.map((n, i) => (
+           <div
+             className="ColorS-grid"
+             style={n ? { background: n } : null}
+             data-index={i}
+             onClick={this.onClick}
+           />
+         ))}
+       </div>
+     );
+   }
+ }
 
 export default App;
